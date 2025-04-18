16a1d8fc236bd04ed30059559cfc66f2d1ebbaa1
nishant-gandhi-17 <64141929+nishant-gandhi-17@users.noreply.github.com>
2025-04-18 20:47:26 -0230

    Create svelte.config.js

---
+++ svelte.config.js
@@ -0,0 +1,9 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+export default {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  }
+};
