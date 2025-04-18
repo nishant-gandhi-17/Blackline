7860fe4e138494afa7d0c2a280554a0eb6a0d1b7
nishant-gandhi-17 <64141929+nishant-gandhi-17@users.noreply.github.com>
2025-04-18 20:27:15 -0230

    Create SportCard.svelte

---
+++ src/lib/components/SportCard.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let name;
+  export let path;
+</script>
+
+<a href={path} class="block bg-white text-black rounded-lg p-4 hover:scale-105 transition text-center">
+  {name}
+</a>
